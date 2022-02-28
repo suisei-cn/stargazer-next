758346d229f2b73a287e2557e6cbcfb3e170fda5
George Miao <gm@miao.dev>
2022-02-28 12:46:28 -0500

    chore: remove windicss

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   swcMinify: true,
-  compress: true,
-  webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin())
-    return config
-  }
+  compress: true
 }
 
 module.exports = nextConfig
