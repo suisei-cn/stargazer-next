91ad22fe4c0f6b4067ae78cca6cc984b5e0206c2
George Miao <gm@miao.dev>
2022-02-28 12:44:26 -0500

    feat: utils

---
+++ src/core/utils.ts
@@ -0,0 +1,35 @@
+import { Name } from './subscribe'
+
+export const regexIndexOf = (
+  string: string,
+  regex: RegExp,
+  startpos?: number
+) => {
+  const pos = startpos === undefined ? 0 : startpos + 1
+  const index = string.slice(pos).search(regex)
+  return index >= 0 ? index + pos : index
+}
+
+export const allIndexOf = (string: string, regex: RegExp) => {
+  let pos: number | undefined = undefined
+  const ret: number[] = []
+  do {
+    pos = regexIndexOf(string, regex, pos)
+    if (pos >= 0) {
+      ret.push(pos)
+    } else {
+      break
+    }
+  } while (true)
+  return ret
+}
+
+// TODO: i18n
+export const getName = (name: Name) => {
+  let ret = name.lang[name.default]
+  if (!ret) throw Error(`Bad name: ${JSON.stringify(name)}`)
+  return ret
+}
+
+export const sleep = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
