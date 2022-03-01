2c5c161839cefe72e6f35e2d12ec1d9a60b2ddde
George Miao <gm@miao.dev>
2022-03-01 18:41:54 -0500

    chore: renaming

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -3,17 +3,24 @@ import type { NextPage } from 'next'
 import { Container } from '@mantine/core'
 
 import SubscribeSearchBar from '@comps/subscribe/searchBar'
-import SubscribeTable from '@comps/subscribe/table'
-import SubscribeBanner from '@comps/subscribe/banner'
+import SubscribeTable from '@comps/subscribe/SubscribeTable'
+import SubscribeBanner from '@comps/subscribe/SubscribeBanner'
+import ScrollToTop from '@comps/ScrollToTop'
 
-const SubscriptionPage: NextPage = () => (
-  <>
-    <SubscribeBanner />
-    <Container size="lg" sx={{ paddingTop: '1.5rem', paddingBottom: '1.5rem' }}>
-      <SubscribeSearchBar />
-      <SubscribeTable />
-    </Container>
-  </>
-)
+const SubscriptionPage: NextPage = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <SubscribeBanner />
+      <Container
+        size="lg"
+        sx={{ paddingTop: '1.5rem', paddingBottom: '1.5rem' }}
+      >
+        <SubscribeSearchBar />
+        <SubscribeTable />
+      </Container>
+    </>
+  )
+}
 
 export default SubscriptionPage
