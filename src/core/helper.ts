6d79faf39d12ff2a486826f114bdf2e71e702ac2
George Miao <gm@miao.dev>
2022-03-01 12:09:29 -0500

    feat: defineVFC supports memo

--- src/core/helper.ts
+++ src/core/helper.ts
@@ -1,4 +1,5 @@
 import type { VFC } from 'react'
+import React from 'react'
 
 export interface baseProp {
   className?: string
@@ -8,14 +9,22 @@ export interface childProp {
   children: React.ReactNode
 }
 
+export interface DefineConfig {
+  memo?: boolean
+}
+
 type Empty = Record<string, never>
 
-export const defineVFC =
-  <P = Empty>(comp: VFC<P & Required<baseProp>>) =>
-  (prop: P & baseProp) => {
+export const defineVFC = <P = Empty>(
+  comp: VFC<P & Required<baseProp>>,
+  config?: DefineConfig
+) => {
+  const ele = (prop: P & baseProp) => {
     const className = prop.className ?? ''
     return comp({ ...prop, className })
   }
+  return config?.memo ? React.memo(ele) : ele
+}
 
 export const defineVFCWithChild = <P = Empty>(
   comp: VFC<childProp & P & Required<baseProp>>
